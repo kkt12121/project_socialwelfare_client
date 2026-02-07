aeeb277d6ae3fd799e409acbdba060ef4b7ca946
kkt12121 <kkt34343@gmail.com>
2026-02-07 17:36:52 +0900

    [2026-02-07]-grade-추가-작업

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "@react-router/dev/config";
+// @ts-ignore
+import { vercelPreset } from "@vercel/react-router/preset";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+  presets: [vercelPreset()],
   ssr: true,
 } satisfies Config;
